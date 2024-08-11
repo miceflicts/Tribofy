f04cbcb4ab8f5bb93535633bc8b8013176dc900f
miceflicts <nickzinhomice@gmail.com>
2024-08-11 17:35:47 -0300

    Feat: Solved not mounted community bug

--- src/app/hooks/use-window-dimensions/index.jsx
+++ src/app/hooks/use-window-dimensions/index.jsx
@@ -4,8 +4,8 @@ import { useState, useEffect } from "react";
 
 function useWindowDimensions() {
   const [windowDimensions, setWindowDimensions] = useState({
-    width: typeof window !== "undefined" ? window.innerWidth : 0,
-    height: typeof window !== "undefined" ? window.innerHeight : 0,
+    width: 0,
+    height: 0,
   });
 
   useEffect(() => {
@@ -16,11 +16,9 @@ function useWindowDimensions() {
       });
     }
 
-    if (typeof window !== "undefined") {
-      window.addEventListener("resize", handleResize);
-      handleResize(); // Call once to set initial dimensions
-      return () => window.removeEventListener("resize", handleResize);
-    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return windowDimensions;
--- src/app/pages/community/page.jsx
+++ src/app/pages/community/page.jsx
@@ -3,11 +3,20 @@
 import useWindowDimensions from "@/app/hooks/use-window-dimensions";
 import CommunityHeader from "@/app/layouts/community/header";
 import LeftSideBar from "@/app/layouts/community/left-sidebar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Community() {
   const { width, height } = useWindowDimensions();
   const [sidebarToggled, setSidebarToggled] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null; // ou um loader/placeholder
+  }
 
   return (
     <>
@@ -17,9 +26,11 @@ export default function Community() {
         ></CommunityHeader>
 
         <div
-          className={`flex h-full w-full ${width <= 900 && sidebarToggled === false ? "hidden" : "flex"}`}
+          className={`h-full w-full ${width >= 1024 ? "flex" : sidebarToggled ? "flex" : "hidden"}`}
         >
-          <LeftSideBar isShowingAddCommunities={false}></LeftSideBar>
+          <LeftSideBar isShowingAddCommunities={true}></LeftSideBar>
+
+          <div className="bg-purple-500"></div>
         </div>
       </div>
     </>
