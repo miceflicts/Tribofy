5acb8a40eb1bda8d49066983ab98cf5b9eb8fa5b
miceflicts <nickzinhomice@gmail.com>
2024-08-11 13:17:17 -0300

    Feat: Community Header documentation / comments

--- src/app/layouts/community/header/index.jsx
+++ src/app/layouts/community/header/index.jsx
@@ -7,13 +7,16 @@ import ThemeToggleButton from "@/app/components/general/theme-toggle-button";
 
 export default function CommunityHeader() {
   return (
+    /* Community page header */
     <header className="flex h-[50px] w-full max-[900px]:h-[64px]">
       <div className="bg-emphasis flex h-full w-full items-center justify-between border-b border-border pl-7 pr-7">
+        {/* Keyboard shortcuts / hamburger div */}
         <div className="flex h-[35px] w-[35px] cursor-pointer items-center justify-center rounded-lg hover:bg-secondary max-[900px]:h-[45px] max-[900px]:w-[45px]">
           <Command className="text-foreground max-[900px]:hidden" size={19} />
           <Menu className="h-[30px] w-[30px] text-foreground min-[900px]:hidden"></Menu>
         </div>
 
+        {/* Search div */}
         <div className="relative max-[900px]:hidden">
           <Search
             className="absolute left-[0.8rem] top-1/2 z-10 -translate-y-1/2 transform text-foreground"
@@ -26,10 +29,12 @@ export default function CommunityHeader() {
           />
         </div>
 
+        {/* Community logo on small devices */}
         <div className="flex h-full w-fit items-center min-[900px]:hidden">
           <h2 className="text-2xl font-bold">Tribofy</h2>
         </div>
 
+        {/* Right items on big screen */}
         <div className="flex items-center gap-6">
           <div className="flex items-center max-[900px]:hidden">
             <ThemeToggleButton />
@@ -45,6 +50,7 @@ export default function CommunityHeader() {
             </div>
           </div>
 
+          {/* User Profile */}
           <div className="h-[34px] w-[34px] rounded-md bg-accent"></div>
         </div>
       </div>
